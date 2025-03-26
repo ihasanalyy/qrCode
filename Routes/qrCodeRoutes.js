2de5a742acaae41fc1e257278d476193919df432
Hassan Ali <ihasanalyy@gmail.com>
2025-03-26 12:30:13 +0500

    get_post

--- Routes/qrCodeRoutes.js
+++ Routes/qrCodeRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import { fetchQrCode, generateQrCode, getQrCode, scanQrCode, getScaannedQrCodes, fetchQrCodesByType, getScansByQrAndCompany } from '../Controllers/qrCodeController.js';
 
-router.get('/fetch/qrcode/:id', fetchQrCode) // Fetch QR code   /////////////////
+router.post('/fetch/qrcode/:id', fetchQrCode) // Fetch QR code   /////////////////
 router.get('/qrcode/details/:id', getQrCode) // Get QR code details    ////////////////
 router.get('/qrcode/scan/:id', scanQrCode) // Scan QR code        ///////////
 router.get('/getscannedviews/:id', getScaannedQrCodes) // Get scanned views
